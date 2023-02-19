54137c25194388686c5ae04723dd48c8f473b396
jkarrer <jkarrer@protect247.com>
2023-02-18 23:06:32 -0500

    :art: Refactor ingredient inputs into seperatecomponent

--- src/lib/components/recipes/create/Ingredients.svelte
+++ src/lib/components/recipes/create/Ingredients.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import Button from '$lib/components/common/Button.svelte';
 	import Paper from '$lib/components/common/Paper.svelte';
 	import Headline from '$lib/components/common/Headline.svelte';
-	import PlusSquareTwoTone from '$lib/assets/icons/PlusSquareTwoTone.svelte';
 	import Checkbox from '$lib/components/common/Checkbox.svelte';
-	import PlusCircleTwoTone from '$lib/assets/icons/PlusCircleTwoTone.svelte';
-	import CloseCircleTwoTone from '$lib/assets/icons/CloseCircleTwoTone.svelte';
-
-	let isCreateIngredientOpen = false;
+	import IngredientInput from './comps/IngredientInput.svelte';
 </script>
 
 <Paper>
@@ -18,37 +13,7 @@
 		<Checkbox />
 	</div>
 
-	<div class="create_wrapper">
-		<button class="create" on:click={() => (isCreateIngredientOpen = !isCreateIngredientOpen)}>
-			<div class="add">
-				<div class="heading">Add Ingredient</div>
-
-				<div class="add_icon">
-					<PlusSquareTwoTone isActive={isCreateIngredientOpen} />
-				</div>
-			</div>
-
-			<div style:display={isCreateIngredientOpen ? 'grid' : 'none'} class="input_wrapper">
-				<input type="text" class="quantity" placeholder="Quantity" />
-				<input type="text" class="unit" placeholder="Unit" />
-				<input type="text" class="name" placeholder="Name" />
-			</div>
-		</button>
-		<div style:display={isCreateIngredientOpen ? 'flex' : 'none'} class="create_buttons">
-			<Button
-				callback={(e) => console.log(e)}
-				text="Commit"
-				icon={PlusCircleTwoTone}
-				width="90px"
-			/>
-			<Button
-				callback={(e) => console.log(e)}
-				text="Erase"
-				icon={CloseCircleTwoTone}
-				width="90px"
-			/>
-		</div>
-	</div>
+	<IngredientInput />
 </Paper>
 
 <style>
@@ -65,69 +30,5 @@
 
 		border: var(--solid-border);
 		border-radius: 3px;
-	}
-
-	.add {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-	}
-	.add_icon {
-		height: 16px;
-		width: 16px;
-	}
-
-	.create {
-		display: grid;
-		align-items: center;
-		grid-auto-flow: row;
-		gap: 20px;
-
-		width: 100%;
-		margin-top: 15px;
-		padding: 12px 15px;
-
-		text-align: left;
-		font-size: var(--rg);
-		color: var(--darker);
-
-		border: var(--dashed-border);
-	}
-
-	.input_wrapper {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		gap: 10px;
-	}
-
-	input {
-		width: 100%;
-		padding: 12px 15px;
-
-		font-size: var(--rg);
-		border-radius: var(--border-radius);
-	}
-	input:focus {
-		border: var(--solid-border);
-		outline: none;
-	}
-	input.name {
-		border: var(--dashed-border);
-		grid-column: 1 / span 2;
-	}
-	input.quantity {
-		border: var(--dashed-border);
-	}
-	input.unit {
-		border: var(--dashed-border);
-	}
-
-	.create_buttons {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		gap: 10px;
-
-		margin-top: 15px;
 	}
 </style>
---
+++ src/lib/components/recipes/create/comps/IngredientInput.svelte
@@ -0,0 +1,94 @@
+<script>
+	import Button from '$lib/components/common/Button.svelte';
+	import PlusSquareTwoTone from '$lib/assets/icons/PlusSquareTwoTone.svelte';
+	import PlusCircleTwoTone from '$lib/assets/icons/PlusCircleTwoTone.svelte';
+	import CloseCircleTwoTone from '$lib/assets/icons/CloseCircleTwoTone.svelte';
+
+	let isActive = false;
+</script>
+
+<button class="root" on:click={() => (isActive = !isActive)}>
+	<div class="title">
+		<div>Add Ingredient</div>
+
+		<div class="title_icon">
+			<PlusSquareTwoTone {isActive} />
+		</div>
+	</div>
+
+	<div style:display={isActive ? 'grid' : 'none'} class="input_wrapper">
+		<input type="text" class="quantity" placeholder="Quantity" />
+		<input type="text" class="unit" placeholder="Unit" />
+		<input type="text" class="name" placeholder="Name" />
+	</div>
+</button>
+
+<div style:display={isActive ? 'flex' : 'none'} class="action_buttons">
+	<Button callback={(e) => console.log(e)} text="Commit" icon={PlusCircleTwoTone} width="90px" />
+	<Button callback={(e) => console.log(e)} text="Erase" icon={CloseCircleTwoTone} width="90px" />
+</div>
+
+<style>
+	.root {
+		display: grid;
+		align-items: center;
+		grid-auto-flow: row;
+		gap: 20px;
+
+		width: 100%;
+		margin-top: 15px;
+		padding: 12px 15px;
+
+		text-align: left;
+		font-size: var(--rg);
+		color: var(--darker);
+
+		border: var(--dashed-border);
+	}
+
+	.title {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.title_icon {
+		height: 16px;
+		width: 16px;
+	}
+
+	.input_wrapper {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 10px;
+	}
+	input {
+		width: 100%;
+		padding: 12px 15px;
+
+		font-size: var(--rg);
+		border-radius: var(--border-radius);
+	}
+	input:focus {
+		border: var(--solid-border);
+		outline: none;
+	}
+	input.name {
+		border: var(--dashed-border);
+		grid-column: 1 / span 2;
+	}
+	input.quantity {
+		border: var(--dashed-border);
+	}
+	input.unit {
+		border: var(--dashed-border);
+	}
+
+	.action_buttons {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+
+		margin-top: 15px;
+	}
+</style>
