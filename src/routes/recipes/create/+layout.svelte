48fe99def1151a76660d180d663f99d3198f41a8
jkarrer <jkarrer@protect247.com>
2023-02-19 23:03:52 -0500

    :sparkles: Add save and reset functions

--- src/routes/recipes/create/+layout.svelte
+++ src/routes/recipes/create/+layout.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
+	import Mobile from '$lib/components/headers/Mobile.svelte';
+	import Button from '$lib/components/common/Button.svelte';
+	import SaveTwoTone from '$lib/assets/icons/SaveTwoTone.svelte';
+	import ReloadOutlined from '$lib/assets/icons/ReloadOutlined.svelte';
+	import BlocksOutlined from '$lib/assets/icons/BlocksOutlined.svelte';
 	import { setContext } from 'svelte';
 	import { writable } from 'svelte/store';
 	import type { Writable } from 'svelte/store';
 	import type { Recipe, RecipeDraft } from '$lib/types/Recipe';
-
-	// * This is the context store for the create recipe page...
-	//... using the layout component as a wrapper, this is a clever way to localize state management.
-	const recipe: Recipe = {
-		title: '',
-		portions: 0,
-		ingredients: [],
-		directions: [],
-		tags: []
-	};
 
 	const recipeDraft: RecipeDraft = {
 		title: '',
@@ -25,11 +20,48 @@
 		tagList: []
 	};
 
-	let store: Writable<RecipeDraft> = writable(recipeDraft);
+	let draftStore: Writable<RecipeDraft> = writable(recipeDraft);
+	setContext<Writable<RecipeDraft>>('recipeDraft', draftStore);
 
-	setContext<Writable<RecipeDraft>>('recipeDraft', store);
+	$: console.log($draftStore);
 
-	$: console.log($store);
+	function saveRecipe() {
+		let recipe: Recipe = {
+			title: $draftStore.title,
+			portions: $draftStore.portions,
+			ingredients: $draftStore.ingredientList,
+			directions: $draftStore.directionList,
+			tags: $draftStore.tagList
+		};
+
+		console.log('FINAL RECIPE', recipe);
+	}
+
+	function saveDraft() {
+		console.log('CURRENT DRAFT', $draftStore);
+	}
+
+	function resetAllValues() {
+		const blankRecipeDraft: RecipeDraft = {
+			title: '',
+			portions: 0,
+			ingredient: { id: '', name: '', unit: '', quantity: '' },
+			ingredientList: [],
+			direction: '',
+			directionList: [],
+			tag: '',
+			tagList: []
+		};
+		draftStore.set(blankRecipeDraft);
+
+		console.log('RESET ALL', $draftStore);
+	}
 </script>
+
+<Mobile>
+	<Button callback={saveRecipe} text="Save" icon={SaveTwoTone} />
+	<Button callback={resetAllValues} text="Reset" icon={ReloadOutlined} />
+	<Button callback={saveDraft} text="Draft" icon={BlocksOutlined} />
+</Mobile>
 
 <slot />
--- src/routes/recipes/create/+page.svelte
+++ src/routes/recipes/create/+page.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-	import Mobile from '$lib/components/headers/Mobile.svelte';
-	import Button from '$lib/components/common/Button.svelte';
-	import SaveTwoTone from '$lib/assets/icons/SaveTwoTone.svelte';
-	import ReloadOutlined from '$lib/assets/icons/ReloadOutlined.svelte';
-	import BlocksOutlined from '$lib/assets/icons/BlocksOutlined.svelte';
 	import Title from '$lib/components/recipes/create/Title.svelte';
 	import Ingredients from '$lib/components/recipes/create/Ingredients.svelte';
 	import Directions from '$lib/components/recipes/create/Directions.svelte';
 	import Tags from '$lib/components/recipes/create/Tags.svelte';
 </script>
-
-<Mobile>
-	<Button callback={(e) => console.log(e)} text="Save" icon={SaveTwoTone} />
-	<Button callback={(e) => console.log(e)} text="Reset" icon={ReloadOutlined} />
-	<Button callback={(e) => console.log(e)} text="Draft" icon={BlocksOutlined} />
-</Mobile>
 
 <div id="recipe_creation_form">
 	<Title />
