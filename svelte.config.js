8116eb8e27b15a10b6178b9000dd50affc30495c
jkarrer <jkarrer@protect247.com>
2023-03-12 08:30:25 -0400

    :wrench: node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
+		//TODO Remove this in production
 		csrf: {
 			checkOrigin: false
 		}
